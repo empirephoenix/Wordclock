dc9589324960fc936f809f1a48948a57dac97c39
ollo <ollo@olloThinkPad>
2016-04-12 20:38:35 +0200

    Unit testing prepared

---
+++ unit/generateTimes.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+# date command is all we need, as shown:
+#date -d '2007-011-01 17:30:24' '+%:z'
+#+01:00
+
+for year in {2016..2016}; do
+ for month in {1..12}; do
+  for day in {1..31}; do
+    hour=$((RANDOM%24))
+    minutes=$((RANDOM%60))
+    seconds=$((RANDOM%60))
+    timestmp="$year-$month-$day $hour:$minutes:$seconds"
+    date -d "$timestmp" "+%F %T" >> /dev/null
+    if [ $? -ne 0 ]; then
+     echo "--Time $timestmp is not valid"
+    else
+     # Result of date looks like +01:00
+     # Grep extracts the hour, then remove the plus in front and at the end remove leading zeros
+     offset=$(date -d "$timestmp" '+%:z' | grep -o "+[0-9]*" | sed 's/+//' | sed 's/^0//')
+     dayofweek=$(date -d "$timestmp" '+%u')
+     # Generate the lua test command, like: checkTime(2015, 1, 1, 10, 11, 12, 0, 1)
+     echo "checkTime($year, $month, $day, $hour, $minutes, $seconds, $dayofweek, $offset)"
+    fi
+  done
+ done
+done
+exit 0
