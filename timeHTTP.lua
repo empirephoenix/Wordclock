028012548b5cb7fb4c01743bebca89b5599c8809
ollo <ollo@olloThinkPad>
2017-01-05 17:27:47 +0100

    A callback is executed, when the webserver gave us the time

--- timeHTTP.lua
+++ timeHTTP.lua
@@ -1,16 +1,31 @@
+-- timeHTTP.lua
 -- Retrieve the current time of a given webserver
 
+-- Some local variables, needed to receive the time
 local httpRequestRunning=false
+local gHour=nil
+local gMin=nil
 
-function getTimeViaHTTP(httpServer, hourOffset)
+-- The function will trigger a callback, when received the answer of the webserver
+-- Example Callback: 
+-- function httpTimeCB(hour, minutes)
+--    print("Received " .. hour .. ":" .. minutes)
+-- end
+-- The Function call would look like the following:
+-- getTimeViaHTTP("www.google.com",0, httpTimeCB)
+function getTimeViaHTTP(httpServer, hourOffset, callback)
     if (hourOffset == nil) then
         hourOffset=0
     end
-
+    collectgarbage()
+    
     httpconn=net.createConnection(net.TCP, 0)
     httpconn:on("disconnection", function(con)
         print("[HTTP] Connection closed")
         httpRequestRunning=false
+        if (callback ~= nil) then
+            callback(gHour, gMin)
+        end
     end)
     httpconn:on("receive", function(con, pl) 
         --print("[HTTP]" .. pl)
@@ -21,14 +36,15 @@ function getTimeViaHTTP(httpServer, hourOffset)
             print("[HTTP] Hour:" .. hour)
             print("[HTTP] Min: " .. min)
             print("[HTTP] Sec: " .. sec)
+            gHour=hour
+            gMin=min
         end
+        httpconn:close()
     end)
     httpconn:connect(80, httpServer)
     httpconn:on("connection", function(sck, c)
       -- Wait for connection before sending.
-      
       httpconn:send("GET / HTTP/1.1\r\nAccept: */*\r\n\r\n")
-      --httpconn:send("GET / HTTP/1.1\r\n\r\n")
       httpRequestRunning=true
       print("[HTTP] Started request " .. httpServer)
     end)
