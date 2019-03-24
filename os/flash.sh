0adb96174ee348b6ac81aca57508044cd7a667b4
ollo <ollo@olloThinkPad>
2019-03-24 22:01:51 +0100

    Updated flashing command

--- os/flash.sh
+++ os/flash.sh
@@ -33,5 +33,4 @@ if [ $? -ne 0 ]; then
 fi
 echo "Flashing the new"
 #./esptool.py --port /dev/$DEVICE $BAUD write_flash -fm dio 0x00000 nodemcu2.bin
-# 0x3fc000 esp_init_data_default_v08.bin 0x07e000 blank.bin 0x3fe000 blank.bin
-./esptool.py --port /dev/$DEVICE $BAUD write_flash 0x00000 0x00000.bin 0x10000 0x10000.bin
+./esptool.py --port /dev/$DEVICE $BAUD write_flash -fm dio 0x00000 0x00000.bin 0x10000 0x10000.bin 0x3fc000 esp_init_data_default.bin 0x07e000 blank.bin 0x3fe000 blank.bin
