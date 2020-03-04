17a9416607199019cd8610e2f3e09de9ab7e96dd
ollo <Ollo>
2020-03-04 21:01:30 +0100

    Set telnet IP address

--- tools/tcpFlash.py
+++ tools/tcpFlash.py
@@ -49,7 +49,7 @@ def main(nodeip, luafile, volatile=None):
     else:
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((nodeip, 80))
+            s.connect((nodeip, 23))
 	    time.sleep(0.050)
             s.sendall("\n")
             # Receive the hello Message of answer of the ESP
